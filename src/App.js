7ce75b1c87621863426021314fda95c48807bdbc
chandlerWTblaschka <chandler.blaschka@watertronics.com>
2022-10-03 10:30:10 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -16,25 +16,25 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Container />} />
+          <Route path="production-tracker/" element={<Container />} />
         </Routes>
         <Routes>
-          <Route path="/2020676" element={<Heritage />} />
+          <Route path="production-tracker/2020676" element={<Heritage />} />
         </Routes>
         <Routes>
-          <Route path="/2020676/OrderSheet" element={<HeritageOrder />} />
+          <Route path="production-tracker/2020676/OrderSheet" element={<HeritageOrder />} />
         </Routes>
         <Routes>
-          <Route path="/2020583" element={<Vanderbilt />} />
+          <Route path="production-tracker/2020583" element={<Vanderbilt />} />
         </Routes>
         <Routes>
-          <Route path="/2020583/OrderSheet" element={<VanderbiltOrder />} />
+          <Route path="production-tracker/2020583/OrderSheet" element={<VanderbiltOrder />} />
         </Routes>
         <Routes>
-          <Route path="/2020580" element={<Lakewood />} />
+          <Route path="production-tracker/2020580" element={<Lakewood />} />
         </Routes>
         <Routes>
-          <Route path="/2020580/OrderSheet" element={<LakewoodOrder />} />
+          <Route path="production-tracker/2020580/OrderSheet" element={<LakewoodOrder />} />
         </Routes>
       </Router>
     </div>

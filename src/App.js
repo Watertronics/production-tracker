5c11365d88e7c6443c20a4b2234302c33ad010f9
chandlerWTblaschka <chandler.blaschka@watertronics.com>
2022-10-03 11:03:56 -0500

    basename

--- src/App.js
+++ src/App.js
@@ -7,36 +7,35 @@ import VanderbiltOrder from './components/VanderbiltOrder'
 import Lakewood from './components/Lakewood'
 import LakewoodOrder from './components/LakewoodOrder'
 import NavBar from "./components/NavBar"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HashRouter from 'react-router-dom'
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="/production-tracker">
         <NavBar />
         <Routes>
-          <Route path="production-tracker/" element={<Container />} />
+          <Route path="/" element={<Container />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020676" element={<Heritage />} />
+          <Route path="/2020676" element={<Heritage />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020676/OrderSheet" element={<HeritageOrder />} />
+          <Route path="/2020676/OrderSheet" element={<HeritageOrder />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020583" element={<Vanderbilt />} />
+          <Route path="/2020583" element={<Vanderbilt />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020583/OrderSheet" element={<VanderbiltOrder />} />
+          <Route path="/2020583/OrderSheet" element={<VanderbiltOrder />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020580" element={<Lakewood />} />
+          <Route path="/2020580" element={<Lakewood />} />
         </Routes>
         <Routes>
-          <Route path="production-tracker/2020580/OrderSheet" element={<LakewoodOrder />} />
+          <Route path="/2020580/OrderSheet" element={<LakewoodOrder />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   )
 }

66ffaa7c879395b110a9be3d60219c2df45fd64c
chandlerWTblaschka <chandler.blaschka@watertronics.com>
2022-10-03 10:17:05 -0500

    heritage

--- src/components/HeritageOrder.js
+++ src/components/HeritageOrder.js
@@ -1,20 +1,28 @@
 import React from "react";
 import '../App.css';
 import SideNav from "./SideNav"
+import genInfo from "../images/heritage-general-info.jpg"
+import invAd from "../images/heritage-invoice-address.jpg"
+import payTerm from "../images/heritage-payment-terms.jpg"
+import shipTo from "../images/heritage-ship-to-address.jpg"
+import priceBreak from "../images/heritage-price-breakdown.jpg"
+import rebate from "../images/heritage-rebate-comm.jpg"
+import comm from "../images/heritage-comm-2-3.jpg"
+import split from "../images/heritage-sales-split.jpg"
 
 function Heritage() {
     return (
         <div className="JobContainer">
             <SideNav />
             <div className="OrderImages">
-                <img src={'../../heritage-general-info.jpg'} className="OrderImage" />
-                <img src={'../../heritage-invoice-address.jpg'} className="OrderImage" />
-                <img src={'../../heritage-payment-terms.jpg'} className="OrderImage" />
-                <img src={'../../heritage-ship-to-address.jpg'} className="OrderImage" />
-                <img src={'../../heritage-price-breakdown.jpg'} className="OrderImage" />
-                <img src={'../../heritage-rebate-comm.jpg'} className="OrderImage" />
-                <img src={'../../heritage-comm-2-3.jpg'} className="OrderImage" />
-                <img src={'../../heritage-sales-split.jpg'} className="OrderImage" />
+                <img src={genInfo} className="OrderImage" />
+                <img src={invAd} className="OrderImage" />
+                <img src={payTerm} className="OrderImage" />
+                <img src={shipTo} className="OrderImage" />
+                <img src={priceBreak} className="OrderImage" />
+                <img src={rebate} className="OrderImage" />
+                <img src={comm} className="OrderImage" />
+                <img src={split} className="OrderImage" />
             </div>
         </div>
     );

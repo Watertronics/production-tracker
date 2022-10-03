3d26cbe3d8230548d9318eeca5318e8c37ef8d89
chandlerWTblaschka <chandler.blaschka@watertronics.com>
2022-10-03 11:07:39 -0500

    fix job list

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -49,7 +49,7 @@ function JobList() {
                         <td>VTV-1-75x4/5ST-460-3-1500-120</td>
                         <td>Greg Salisbury</td>
                         <td>Job Released</td>
-                        <td className="CenterColumn"><Link to={`/2020583`} className="EditLink" ><BsFillPencilFill /></Link></td>
+                        <td className="CenterColumn"><Link to={`/production-tracker/2020583`} className="EditLink" ><BsFillPencilFill /></Link></td>
                     </tr>
                     <tr>
                         <td className="CenterColumn">2020580</td>
@@ -57,7 +57,7 @@ function JobList() {
                         <td>VTV-1-60x3/15V/5ST-460-3-1800-120</td>
                         <td>Mike Green</td>
                         <td>Job Released</td>
-                        <td className="CenterColumn" ><Link to={`/2020580`} className="EditLink" ><BsFillPencilFill /></Link></td>
+                        <td className="CenterColumn" ><Link to={`/production-tracker/2020580`} className="EditLink" ><BsFillPencilFill /></Link></td>
                     </tr>
                     {/* <tr>
                         <td className="CenterColumn" colSpan={6}>

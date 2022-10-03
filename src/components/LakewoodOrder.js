e8d1781fe1d7f98962f3691993c9016f0bc47666
chandlerWTblaschka <chandler.blaschka@watertronics.com>
2022-10-03 11:17:56 -0500

    order

--- src/components/LakewoodOrder.js
+++ src/components/LakewoodOrder.js
@@ -1,17 +1,22 @@
 import React from "react";
 import '../App.css';
 import SideNav from "./SideNav"
+import genPay from "../images/lakewood-general-payment-terms.jpg"
+import ship from "../images/lakewood-ship-price.jpg"
+import rebate from "../images/lakewood-rebate-comm2.jpg"
+import comm from "../images/lakewood-comm3.jpg"
+import split from "../images/lakewood-sales-split.jpg"
 
 function Lakewood() {
     return (
         <div className="JobContainer">
             <SideNav />
             <div className="OrderImages">
-                <img src={'../../lakewood-general-payment-terms.jpg'} className="OrderImage" />
-                <img src={'../../lakewood-ship-price.jpg'} className="OrderImage" />
-                <img src={'../../lakewood-rebate-comm2.jpg'} className="OrderImage" />
-                <img src={'../../lakewood-comm3.jpg'} className="OrderImage" />
-                <img src={'../../lakewood-sales-split.jpg'} className="OrderImage" />
+                <img src={genPay} className="OrderImage" />
+                <img src={ship} className="OrderImage" />
+                <img src={rebate} className="OrderImage" />
+                <img src={comm} className="OrderImage" />
+                <img src={split} className="OrderImage" />
             </div>
         </div>
     );
--- src/components/VanderbiltOrder.js
+++ src/components/VanderbiltOrder.js
@@ -1,17 +1,22 @@
 import React from "react";
 import '../App.css';
 import SideNav from "./SideNav"
+import genPay from "../images/vanderbilt-general-payment-terms.jpg"
+import ship from "../images/vanderbilt-ship-price.jpg"
+import rebate from "../images/vanderbilt-rebate-comm2.jpg"
+import comm from "../images/vanderbilt-comm3.jpg"
+import split from "../images/vanderbilt-sales-split.jpg"
 
 function Vanderbilt() {
     return (
         <div className="JobContainer">
             <SideNav />
             <div className="OrderImages">
-                <img src={'../../vanderbilt-general-payment-terms.jpg'} className="OrderImage" />
-                <img src={'../../vanderbilt-ship-price.jpg'} className="OrderImage" />
-                <img src={'../../vanderbilt-rebate-comm2.jpg'} className="OrderImage" />
-                <img src={'../../vanderbilt-comm3.jpg'} className="OrderImage" />
-                <img src={'../../vanderbilt-sales-split.jpg'} className="OrderImage" />
+                <img src={genPay} className="OrderImage" />
+                <img src={ship} className="OrderImage" />
+                <img src={rebate} className="OrderImage" />
+                <img src={comm} className="OrderImage" />
+                <img src={split} className="OrderImage" />
             </div>
         </div>
     );
